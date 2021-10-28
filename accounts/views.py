9a9fa775fc1d0b7d0effea1226c757de8bbe5ec8
Wander Moreira <wandersantosmoreira@gmail.com>
2021-10-28 13:57:25 -0300

    feat: routes to create a user

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,11 @@ from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 
 class UserCreateView(APIView):
   def post(self, request):
-    print(request.data)
-    # User.objects.create_user(
-    #   username=request.data['username'],
-    #   password=request.data['password'])
+    User.objects.create_user(request.data)
 
-    return '',200
+    return Response({'msg':'ok'})
